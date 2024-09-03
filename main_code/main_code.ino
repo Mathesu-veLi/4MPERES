2f710994e7b39c0fe43c0a04ee5b3a1d653ff30e
Matheus <matheuslevit@gmail.com>
2024-09-03 20:24:19 -0300

    uses the DualMotors library

--- main_code/main_code.ino
+++ main_code/main_code.ino
@@ -1,58 +1,20 @@
-#include <Motor.h>
+#include <DualMotor.h>
 
-class Motors {
-  private:
-    Motor* motor1;
-    Motor* motor2;
-  public:
-    void setMotor1(int pinL, int pinR) {
-      motor1 = new Motor(pinL, pinR);
-    };
-
-    void setMotor2(int pinL, int pinR) {
-      motor2 = new Motor(pinL, pinR);
-    };
-
-    void forward() {
-      motor1->forward();
-      motor2->forward();
-    };
-
-    void left() {
-      motor1->back();
-      motor2->forward();
-    }
-
-    void right() {
-      motor1->forward();
-      motor2->back();
-    }
-
-    void back() {
-      motor1->back();
-      motor2->back();
-    };
-
-    void stop() {
-      motor1->stop();
-      motor2->stop();
-    };
-};
-
-Motors frontalMotors;
+// Creates an instance of motors class with their default speed 
+DualMotor* frontalMotors = new DualMotor(127);
 
 void setup() {
-  // Set motors pins
-  frontalMotors.setMotor1(8, 9);
-  frontalMotors.setMotor2(10, 11);
+	// Set motors pins
+  frontalMotors->setMotor1(8, 9);
+  frontalMotors->setMotor2(10, 11);
 }
 
 void loop() {
 	// Makes the motors go to forward
-  frontalMotors.forward();
+  frontalMotors->forward();
   delay(1000);
   
   // Makes the motors go to back
-  frontalMotors.back();
+  frontalMotors->back();
   delay(1000);
 }
