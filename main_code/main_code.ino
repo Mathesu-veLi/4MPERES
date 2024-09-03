5173a64cc0ed08193bfd6b017b7e91969c02555b
Matheus <matheuslevit@gmail.com>
2024-09-03 20:44:30 -0300

    makes motors go to forward when has line in center

--- main_code/main_code.ino
+++ main_code/main_code.ino
@@ -1,20 +1,36 @@
 #include <DualMotor.h>
 
-// Creates an instance of motors class with their default speed 
+// Line sensors pins
+const int liners[] = {2, 3};
+
+// Creates an instance of motors class with their default speed
 DualMotor* frontalMotors = new DualMotor(127);
 
 void setup() {
 	// Set motors pins
-  frontalMotors->setMotor1(8, 9);
-  frontalMotors->setMotor2(10, 11);
+  frontalMotors.setMotor1(8, 9);
+  frontalMotors.setMotor2(10, 11);
+	
+	// Set line sensors as input
+  for (int ls : liners) {
+    pinMode(ls, INPUT);
+  }
+}
+
+bool hasLine(int liner) {
+  return digitalRead(liner);
 }
 
 void loop() {
-	// Makes the motors go to forward
-  frontalMotors->forward();
-  delay(1000);
+  bool hasLineInLeft = hasLine(liners[0]);
+  bool hasLineInRight = hasLine(liners[1]);
+  bool hasLineInCenter = !hasLineInLeft && !hasLineInRight;
   
-  // Makes the motors go to back
-  frontalMotors->back();
-  delay(1000);
+  if(hasLineInCenter) {
+    // Makes motors go to forward
+    frontalMotors.forward();
+  }
+  
+  delay(100);
 }
+
