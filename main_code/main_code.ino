8e5486ec2fb77e43825e34cb674e5f543e53d6e4
Matheus <matheuslevit@gmail.com>
2024-09-03 20:52:31 -0300

    creates a simple line follower code

--- main_code/main_code.ino
+++ main_code/main_code.ino
@@ -8,8 +8,8 @@ DualMotor* frontalMotors = new DualMotor(127);
 
 void setup() {
 	// Set motors pins
-  frontalMotors.setMotor1(8, 9);
-  frontalMotors.setMotor2(10, 11);
+  frontalMotors->setMotor1(8, 9);
+  frontalMotors->setMotor2(10, 11);
 	
 	// Set line sensors as input
   for (int ls : liners) {
@@ -26,9 +26,19 @@ void loop() {
   bool hasLineInRight = hasLine(liners[1]);
   bool hasLineInCenter = !hasLineInLeft && !hasLineInRight;
   
+  // Makes motors go to forward when no has line in left and right
   if(hasLineInCenter) {
-    // Makes motors go to forward
-    frontalMotors.forward();
+    frontalMotors->forward();
+  }
+  
+  // Makes motors go to left when has line in left
+  if (hasLineInLeft) {
+    frontalMotors->left();
+  }
+  
+  // Makes motors go to right when has line in right
+  if (hasLineInRight) {
+    frontalMotors->right();
   }
   
   delay(100);
