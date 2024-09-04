793915865aef167c6660399af27449c335468c96
Matheus <matheuslevit@gmail.com>
2024-09-03 21:26:14 -0300

    creates a line follower with PID

--- main_code/main_code.ino
+++ main_code/main_code.ino
@@ -1,46 +1,85 @@
 #include <DualMotor.h>
+#include <QTRSensors.h>
 
-// Line sensors pins
-const int liners[] = {2, 3};
+#define DEFAULT_SPEED 127
+#define MAX_SPEED 255
 
 // Creates an instance of motors class with their default speed
-DualMotor* frontalMotors = new DualMotor(127);
+DualMotor* frontalMotors = new DualMotor(DEFAULT_SPEED);
+
+// Creates an intance of sensors class
+QTRSensors qtr;
+
+const uint8_t SensorCount = 5;
+uint16_t sensorValues[SensorCount];
+
+// PID
+int lastError = 0;
+int P;
+int I;
+int D;
+
+float Kp = 55;
+float Kd = 30;
+float Ki = 0.00;
 
 void setup() {
 	// Set motors pins
   frontalMotors->setMotor1(8, 9);
   frontalMotors->setMotor2(10, 11);
 	
-	// Set line sensors as input
-  for (int ls : liners) {
-    pinMode(ls, INPUT);
-  }
+	// Set line sensors pins
+	qtr.setTypeRC();
+  qtr.setSensorPins((const uint8_t[]){A1, A2, A3, A4, A5}, SensorCount);
+  
+  calibrateSensors();
+  delay(7000);
 }
 
-bool hasLine(int liner) {
-  return digitalRead(liner);
+void calibrateSensors() {
+	for (uint16_t i = 0; i < 50; i++)
+  {
+    qtr.calibrate();
+  }
 }
 
 void loop() {
-  bool hasLineInLeft = hasLine(liners[0]);
-  bool hasLineInRight = hasLine(liners[1]);
-  bool hasLineInCenter = !hasLineInLeft && !hasLineInRight;
-  
-  // Makes motors go to forward when no has line in left and right
-  if(hasLineInCenter) {
-    frontalMotors->forward();
-  }
-  
-  // Makes motors go to left when has line in left
-  if (hasLineInLeft) {
-    frontalMotors->left();
-  }
-  
-  // Makes motors go to right when has line in right
-  if (hasLineInRight) {
-    frontalMotors->right();
-  }
-  
+	int correctionSpeed = calculateCorrection();
+	
+	int speedA = DEFAULT_SPEED + correctionSpeed;
+  int speedB = DEFAULT_SPEED - correctionSpeed;
+  speedConstrains(speedA, speedB);
+	
+  frontalMotors->custom(speedA, speedB);
   delay(100);
 }
 
+int calculateCorrection(){
+  uint16_t position = qtr.readLineBlack(sensorValues);
+  int error = 2500 - position;
+
+  P = error;
+  I = I + error;
+  D = error - lastError;
+  lastError = error;
+
+	return (Kp * P) + (Ki * I) + (Kd * D);  
+}
+
+void speedConstraints(int speedA, int speedB) {
+	if(speedA > MAX_SPEED){
+    speedA = MAX_SPEED;
+  }
+
+  if(speedB > MAX_SPEED){
+    speedB = MAX_SPEED;
+  }
+
+  if(speedA < 0){
+    speedA = 0;
+  }
+
+  if(speedB < 0){
+    speedB = 0;
+  }	
+}
